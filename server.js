b826d3a1a5f0ba524797466617b369952bbfe583
manisha332 <manishakp135@gmail.com>
2026-07-06 08:21:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
-require("dotenv").config();
+
 
 const connectDB = require("./db");
 const user = require("./user");
